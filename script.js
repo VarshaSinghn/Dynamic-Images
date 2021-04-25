b8ef40a1d8222e4d87eebd4a6917ac0cec3cca20
Varsha Rani N <SinghVarsha.N1994@gmail.com>
2021-04-26 02:32:58 +0530

    Added dynamic changes

--- script.js
+++ script.js
@@ -1,3 +1,126 @@
+// To Display list of doctors......
+
+const docData = newDoctorData (),
+
+      containers = document.getElementById ( `doctor` );
+
+docData.forEach ( ( res ) => {
+  // Create card element
+  const card = document.createElement ( `div` );
+
+  card.classList = `card-body`;
+
+  //  Card content
+  const content = `
+  <div class="childDoctor">
+
+									<h5 class="font-weight-bold docHeading mt-2 mb-4">${ res.template }</h5>
+									<img src="${ res.image }" alt="" class="rounded mx-auto" />
+									<h3 class="mt-3 nameHead">${ res.name }</h3>
+									<p class="para1">${ res.degree }</p>
+									<p class="para2">${ res.dept }</p>
+									<p class="line m-auto"></p>
+										<p class="doctorDesc mt-2">
+											${ res.statement }
+										</p>
+									<div>
+									<button class="btn btn-primary mt-3 mb-4 knowMore-Btn mt-3">${ res.button }</button>
+									</div>
+
+
+							</div>
+  `;
+
+  // Append newyly created card element to the container
+  containers.innerHTML += content;
+} );
+
+// To Display Warning Sign......
+
+const warnData = warningSign (),
+
+      containerss = document.getElementById ( `warningSign` );
+
+warnData.forEach ( ( ress ) => {
+  // Create card element
+  const card = document.createElement ( `div` );
+
+  card.classList = `card-body`;
+
+  //  Card content
+  const content = `
+  <div class="warningSigns col-3 ml-5">
+									<img src="${ ress.image }" alt="Card image cap" width="" height="45px" class="ml-4 mt-4" />
+									<p class="warningPara mt-2">${ ress.text }</p>
+								</div>
+  `;
+
+  // Append newyly created card element to the container
+  containerss.innerHTML += content;
+} );
+
+// To Display bone Narrow......
+
+const boneMarrowData = boneMarrow (),
+      warn = document.getElementById ( `boneMarrow` );
+
+console.log ( boneMarrowData.length );
+
+// console.log ( response.boneMarrowData.id );
+const arr = boneMarrowData.map ( ( e ) => {
+  console.log ( e.id );
+} );
+
+boneMarrowData.forEach ( ( warning ) => {
+  // Create card element
+  const card = document.createElement ( `div` );
+
+  card.classList = `card-body`;
+
+  //  Card content
+  const content = `
+		<div>
+
+								<p class="ques marrowTransplant" id="bone${ warning.id }">${ warning.question }
+								<span class="float-right"><img src="images/invertedTriangle.png" alt="" id="image${ warning.id }" /></span></p>
+
+								<hr>
+
+								<div id="hidePara${ warning.id }">
+								<p class="warningPara mt-2 marrowTransplantDesc">${ warning.text }</p>
+								</div>
+		</div>
+  `;
+
+  // Append newyly created card element to the container
+  warn.innerHTML += content;
+} );
+// $ ( `.marrowTransplant` )
+//   .click ( () => {
+//     $ ( `.marrowTransplantDesc` )
+//       .toggle ();
+//   } );
+
+$ ( `#boneMarrow` )
+  .click ( ( event ) => {
+    const boneEvent = event.target.id;
+
+    // console.log ( boneEvent );
+
+    for ( const bone of boneMarrowData ) {
+      if ( boneEvent === `bone${ bone.id }` || boneEvent === `image${ bone.id }` ) {
+        $ ( `#hidePara${ bone.id }` )
+        	.toggle ();
+
+        // $ ( `#image` )
+        //   .target ();
+        console.log ( `hello` );
+      }
+    }
+  } );
+
+// list of testimonials
+
 const empData = newData (),
 
       container = document.getElementById ( `accordion` );
